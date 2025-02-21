0e0a1ff18269e02492d188bf372f2827ffdef8d0
AlexDorofeeva <alexaleksandradorofeeva@gmail.com>
2025-02-21 15:40:57 +0800

    Added solution by Alex

--- js homework team/homework 4/4.(2,3,4).js
+++ js homework team/homework 4/4.(2,3,4).js
@@ -17,6 +17,15 @@ console.log(Math.min(...arr));
 
 console.log(Math.max(...arr));
 
+// 4
+let a = [[1, 2], [3, 4, 5], [6, 7, 8]] ;
+let sum = 0;
+a.forEach(x => {
+    sum += x;
+});
+console.log(sum);
+
+
 
 
 
