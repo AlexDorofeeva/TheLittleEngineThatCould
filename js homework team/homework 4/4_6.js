ca5412d3ffbddf8acfc483ee3faa85ee225c7401
BonumTellurian <pavel.shtykh@mail.ru>
2025-02-24 18:01:57 +0300

    hw4.6

--- js homework team/homework 4/4_6.js
+++ js homework team/homework 4/4_6.js
@@ -4,7 +4,20 @@
 
 
 // ====================================================================================================================================================
-// Name: Alex
+// Name: Pavel Sht
+
+const heightWall = 5;
+const climbPerDay = 3;
+const slidePerNight = 2;
+
+let result = 0;
+let day = 1;
+
+while (result < (heightWall - climbPerDay)){
+    day++;
+    result = result + climbPerDay - slidePerNight;
+}
+console.log(`Улитка подниметься за ${day} дня`)
 
 
 
@@ -17,46 +30,48 @@
 
 
 
-// ====================================================================================================================================================
-// Name: Alina
 
-let wall = 5; // Высота 
-let climbPerDay = 3; // Улитка поднимается за день
-let slidePerNight = 2; // Улитка сползает за ночь
 
-for (let position = 0, days = 1; ; days++) {
-    position += climbPerDay; // Улитка ползёт вверх
+// // ====================================================================================================================================================
+// // Name: Alina
 
-    if (position >= wall) { // Если достигла 5 метров, то выводим ответ. Если нет, то производим следующее действие ниже
-        console.log(days);
-        break;
-    } 
+// let wall = 5; // Высота 
+// let climbPerDay = 3; // Улитка поднимается за день
+// let slidePerNight = 2; // Улитка сползает за ночь
 
-    position -= slidePerNight; // Улитка сползает вниз ночью
+// for (let position = 0, days = 1; ; days++) {
+//     position += climbPerDay; // Улитка ползёт вверх
+
+//     if (position >= wall) { // Если достигла 5 метров, то выводим ответ. Если нет, то производим следующее действие ниже
+//         console.log(days);
+//         break;
+//     } 
+
+//     position -= slidePerNight; // Улитка сползает вниз ночью
 
     
-}
+// }
 
-// ====================================================================================================================================================
-// Name: Slava
+// // ====================================================================================================================================================
+// // Name: Slava
 
-let wallHeight = 5;
-let dayUpDistance = 3;
-let nightDownDistance =2;
-let days = 0;
-let distance = 0;
+// let wallHeight = 5;
+// let dayUpDistance = 3;
+// let nightDownDistance =2;
+// let days = 0;
+// let distance = 0;
 
-for (let i = 0; i < wallHeight; i++) {
-    days++;
-    distance += dayUpDistance;
-    if (distance < wallHeight) {
-        distance -= nightDownDistance;
-    } else {
-        console.log(days);
-        break;
-    }
-}
+// for (let i = 0; i < wallHeight; i++) {
+//     days++;
+//     distance += dayUpDistance;
+//     if (distance < wallHeight) {
+//         distance -= nightDownDistance;
+//     } else {
+//         console.log(days);
+//         break;
+//     }
+// }
 
-// ====================================================================================================================================================
-// Name: 
+// // ====================================================================================================================================================
+// // Name: 
 
