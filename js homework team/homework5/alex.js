5a379cb02a9f04e18364e5ffc6afeb78ec42b982
AlexDorofeeva <alexaleksandradorofeeva@gmail.com>
2025-02-26 10:06:12 +0800

    new homework uploaded

---
+++ js homework team/homework5/alex.js
@@ -0,0 +1,9 @@
+// 5.	Креативное задание 1*
+
+// Имеется зашифрованное предложение 
+
+// “Ybrobrubr brabrrbrebr brtbrhbrebr brbbrebrsbrtbr brQbrAbr brebrvbrebrrbr”
+
+// Мы знаем, что оно означает “You are the best QA ever”.
+// Нам нужно придумать программу дешифратор.
+
