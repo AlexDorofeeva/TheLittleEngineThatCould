c4dd8731cdaa6dedc208ce1d0a56e1ffe562b311
AlexDorofeeva <alexaleksandradorofeeva@gmail.com>
2025-02-28 18:12:01 +0800

    Added solution by Alex

---
+++ js homework team/homework5/dima.js
@@ -0,0 +1,6 @@
+Циклы
+https://www.w3schools.com/js/js_loop_while.asp  
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while  
+https://learn.javascript.ru/while-for  (секция о цикле “while” и “do…while”)
+
+dsjkhfdkjhflkdshflkhfjesd
