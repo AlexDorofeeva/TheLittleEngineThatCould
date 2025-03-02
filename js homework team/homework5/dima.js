1a21f844eb1868779eb8d176c90991af8f4963f0
AlexDorofeeva <alexaleksandradorofeeva@gmail.com>
2025-03-02 12:10:56 +0800

    added by Alex

--- js homework team/homework5/dima.js
+++
@@ -1,6 +0,0 @@
-Циклы
-https://www.w3schools.com/js/js_loop_while.asp  
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while  
-https://learn.javascript.ru/while-for  (секция о цикле “while” и “do…while”)
-
-dsjkhfdkjhflkdshflkhfjesd
