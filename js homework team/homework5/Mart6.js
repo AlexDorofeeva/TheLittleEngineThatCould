73f0207c0d5e31883db3b01bb5b590fb88318f3a
al-mart-cheshi <al.martyanov19@gmail.com>
2025-03-15 17:59:43 +0300

    ку

---
+++ js homework team/homework5/Mart6.js
@@ -0,0 +1,35 @@
+// function greet(name) {
+//     if (name === undefined) {
+//         return "Hello, stranger!";
+//     }
+//     else {
+//         return "Hello, " + name + "!";
+//     }
+// }
+// console.log(greet());
+
+// function Greetings(name) {
+//     console.log( `Привет ${name} `);
+// }
+// Greetings('Bvz')
+
+
+// function name(arrNum) {
+//     let res = arrNum.reduce(a)
+// }
+
+// let str  = 'asdf'
+// str[0] = '$'
+// console.log(str[0]);
+
+
+
+
+/*
+ Дан некоторый массив, например, вот такой:
+
+[1, 2, 3, 4, 5, 6]
+Поделите сумму элементов, стоящих на четных позициях, на сумму элементов, стоящих на нечетных позициях.
+ */
+
+
